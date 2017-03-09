e9359a3fc395c506bb3bd3b98338c4a14ebc64db
mehmet <mehmetugur@live.com>
2017-03-09 20:42:22 +0300

    first upload

---
+++ HotDealsUKApiDemo/Controllers/DealController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace HotDealsUKApiDemo.Controllers
+{
+    public class DealController : ApiController
+    {
+        private List<Deal> FindAllDeals()
+		{
+			List<Deal> dealList = new List<Deal>();
+
+			for (int i = 0; i < 10; i++)
+			{
+				Deal currentDeal = new Deal();
+				currentDeal.ID = i;
+				currentDeal.Name = i.ToString() + "- Deal";
+				dealList.Add(currentDeal);
+			}
+
+			return dealList;
+		}
+		public List<Deal> Get()
+		{
+			return FindAllDeals();
+		}
+		public Deal Get(int id)
+		{
+			return FindAllDeals()[id];
+		}
+    }
+}
---
+++ HotDealsUKApiDemo/Models/Deal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HotDealsUKApiDemo
+{
+	public class Deal
+	{
+		public Deal()
+		{
+		}
+
+		public int ID
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+	}
+}
